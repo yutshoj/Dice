a509dc7c0fc60728b8581bd38506f7dc516fdb2f
SHOJI Yuta <s1320081@sshsv211.u-aizu.ac.jp>
2025-07-02 11:47:45 +0900

    Won/lost message added

--- dice_game.c
+++ dice_game.c
@@ -6,11 +6,18 @@ int main() {
     srand(time(NULL));
     int die1 = rand() % 6 + 1;
     int die2 = rand() % 6 + 1;
+    int total = die1 + die2;
 
     printf("Rolling dice...\n");
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
-    printf("Total value: %d\n", die1 + die2);
+    printf("Total value: %d\n", total);
+
+    if (total > 7) {
+        printf("You won!\n");
+    } else {
+        printf("You lost!\n");
+    }
 
     return 0;
 }
