cf857c1fda19fa1695c7150c8d2b61ed83e8270b
SHOJI Yuta <s1320081@sshsv211.u-aizu.ac.jp>
2025-07-02 11:51:32 +0900

    Won/lost message modified

--- dice_game.c
+++ dice_game.c
@@ -20,9 +20,9 @@ int main() {
     printf("Total value: %d\n", total);
 
     if (total > 7) {
-        printf("You won!\n");
+        printf("%s won!\n", name);
     } else {
-        printf("You lost!\n");
+        printf("%s lost!\n", name);
     }
 
     return 0;
